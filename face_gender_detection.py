7a440af5dea060248116c2e0df1d6883a6ccd997
harrywenjie <harrywenjie@gmail.com>
2023-05-05 23:06:28 +1200

    modified:   face_gender_detection.py

--- face_gender_detection.py
+++ face_gender_detection.py
@@ -3,21 +3,30 @@ import cvlib as cv
 from cvlib.object_detection import draw_bbox
 
 def detect_faces_and_gender(image):
-    faces, _ = cv.detect_face(image)
+    faces, confidences = cv.detect_face(image)
     face_gender_data = []
 
-    for face in faces:
-        start_x, start_y, end_x, end_y = face
-        face_crop = image[start_y:end_y, start_x:end_x]
-        
+    padding = 20
+
+    for i, face in enumerate(faces):
+        startX, startY, endX, endY = face
+        startX, startY = max(0, startX - padding), max(0, startY - padding)
+        endX, endY = min(image.shape[1] - 1, endX + padding), min(image.shape[0] - 1, endY + padding)
+
+        face_crop = image[startY:endY, startX:endX]
+
         label, confidence = cv.detect_gender(face_crop)
         idx = confidence.index(max(confidence))
         gender = label[idx]
 
         face_data = {
-            'bounding_box': [start_x, start_y, end_x - start_x, end_y - start_y],
+            'bounding_box': [startX, startY, endX - startX, endY - startY],
             'gender': gender
         }
         face_gender_data.append(face_data)
+
+        print(f"Face {i + 1}:")
+        print(f"  Bounding box: {face_data['bounding_box']}")
+        print(f"  Gender: {face_data['gender']}")
 
     return face_gender_data
